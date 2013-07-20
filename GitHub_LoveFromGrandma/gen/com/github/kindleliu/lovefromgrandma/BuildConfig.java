e6662458b7d027941440720dafd9a165c0ca0394
kindleLiu <ft2028739@gmail.com>
2013-07-20 13:47:53 +0800

    上传Android工程
    尚无任何内容

---
+++ GitHub_LoveFromGrandma/gen/com/github/kindleliu/lovefromgrandma/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.github.kindleliu.lovefromgrandma;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
